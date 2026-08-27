1d7823a61e04f1df4782fb30bfa53c1ab4015c16
Jose Contreras <jose.contreras.ca@tecsup.edu.pe>
2026-08-27 00:47:29 -0500

    respuesta val vs var

--- Semana 02/Carrito/src/main/java/com/contreras/carrito/Carrito.kt
+++ Semana 02/Carrito/src/main/java/com/contreras/carrito/Carrito.kt
@@ -1,7 +1,30 @@
 package com.contreras.carrito
 
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
 fun main() {
     println("=========================================")
-    println(" CARRITO DE COMPRAS- TIENDA TECSUP ")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
     println("=========================================")
+
+    val nombreCliente = "Jose Contreras"
+    val carrito = mutableListOf<Producto>()
+
+    println("Cliente: $nombreCliente")
+    println()
+
+
+    carrito.add(Producto("Laptop HP", 2500.0, 1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 2))
+    carrito.add(Producto("Teclado Mecánico", 120.0, 1))
+    carrito.add(Producto("Monitor Samsung", 800.0, 2)) // Este tiene cantidad > 1
+
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
 }
