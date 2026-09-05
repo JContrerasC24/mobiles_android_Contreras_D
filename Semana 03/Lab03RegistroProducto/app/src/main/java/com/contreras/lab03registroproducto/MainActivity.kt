42194d0a65f61f070a4ba00dc8efd4be507749ec
Jose Contreras Cabrera <jose.contreras.ca@tecsup.edu.pe>
2026-09-05 10:17:26 -0500

    Agrega encabezado con jerarquia tipografica

--- Semana 03/Lab03RegistroProducto/app/src/main/java/com/contreras/lab03registroproducto/MainActivity.kt
+++ Semana 03/Lab03RegistroProducto/app/src/main/java/com/contreras/lab03registroproducto/MainActivity.kt
@@ -12,36 +12,48 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.contreras.lab03registroproducto.ui.theme.Lab03RegistroProductoTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Lab03RegistroProductoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PantallaRegistro(
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Lab03RegistroProductoTheme {
-        Greeting("Android")
+fun PantallaRegistro(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Nuevo producto",
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Text(
+            text = "Completa los datos y presiona Agregar",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.outline
+        )
+        Spacer(modifier = Modifier.height(24.dp))
     }
 }
